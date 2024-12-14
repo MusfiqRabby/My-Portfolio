b0634242713d15c776344c44f17f5d6a9bb4b9bd
musfiqrabby <musfiqrabby69@gmail.com>
2024-12-14 21:50:16 +0600

    first commit

---
+++ my-project/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Navbar = () => {
+    return (
+       <header className="sticky top-0 z-50 w-full border-b bg-backround/95 ">
+
+       </header>
+    );
+};
+
+export default Navbar;
